1d2ca72183f9e3020edf4bfdde5d5ee582b5284f
Saviskar <Saviskar>
2024-10-25 22:27:45 +0530

    Started create customer page

--- src/DashboardSidePanel.jsx
+++ src/DashboardSidePanel.jsx
@@ -1,4 +1,71 @@
-export default function Login() {
+function CreateCustomer() {
+  return (
+    <section class="bg-gray-2 rounded-xl">
+      <div class="p-8 shadow-lg">
+        <form class="space-y-4">
+          <div class="w-full">
+            <label class="sr-only" for="name">
+              Name
+            </label>
+            <input
+              class="input input-solid max-w-full"
+              placeholder="Name"
+              type="text"
+              id="name"
+            />
+          </div>
+
+          <div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
+            <div>
+              <label class="sr-only" for="email">
+                Email
+              </label>
+              <input
+                class="input input-solid"
+                placeholder="Email address"
+                type="email"
+                id="email"
+              />
+            </div>
+
+            <div>
+              <label class="sr-only" for="phone">
+                Phone
+              </label>
+              <input
+                class="input input-solid"
+                placeholder="Phone Number"
+                type="tel"
+                id="phone"
+              />
+            </div>
+          </div>
+
+          <div class="w-full">
+            <label class="sr-only" for="message">
+              Message
+            </label>
+
+            <textarea
+              class="textarea textarea-solid max-w-full"
+              placeholder="Message"
+              rows="8"
+              id="message"
+            ></textarea>
+          </div>
+
+          <div class="mt-4">
+            <button type="button" class="rounded-lg btn btn-primary btn-block">
+              Send Enquiry
+            </button>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+}
+
+export default function DashboardSidePanel() {
   return (
     <div className="flex flex-row sm:gap-10">
       <div className="sm:w-full sm:max-w-[18rem]">
@@ -35,7 +102,7 @@ export default function Login() {
                     <span>General</span>
                   </li>
 
-                  <li className="menu-item menu-active">
+                  <li className="menu-item">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       height="24px"
@@ -59,6 +126,30 @@ export default function Login() {
                     </svg>
                     <span>View Completed Orders</span>
                   </li>
+                  <li className="menu-item">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      height="24px"
+                      viewBox="0 -960 960 960"
+                      width="24px"
+                      fill="#b8b8b8"
+                    >
+                      <path d="M567.31-685.39v-59.99h300v59.99h-300Zm0 155.39v-60h300v60h-300Zm0 155.38v-59.99h300v59.99h-300ZM317.4-407.69q-43.55 0-74.13-30.49t-30.58-74.04q0-43.55 30.49-74.12 30.49-30.58 74.04-30.58 43.55 0 74.13 30.48 30.57 30.49 30.57 74.04 0 43.55-30.48 74.13-30.49 30.58-74.04 30.58ZM92.69-183.08v-60.61q0-17.03 8.27-32.44 8.27-15.41 22.81-24.48 43.46-25.47 91.73-38.58 48.28-13.12 101.81-13.12t101.8 13.12q48.27 13.11 91.74 38.58 14.53 9.07 22.8 24.48 8.27 15.41 8.27 32.44v60.61H92.69Zm63.69-66.15v6.15h321.85v-6.15q-37.31-21.16-78.04-32.12-40.73-10.96-82.88-10.96-42.16 0-82.89 10.96-40.73 10.96-78.04 32.12Zm160.93-218.46q18.54 0 31.57-13.04 13.04-13.04 13.04-31.58t-13.04-31.57q-13.03-13.04-31.57-13.04t-31.58 13.04q-13.04 13.03-13.04 31.57t13.04 31.58q13.04 13.04 31.58 13.04Zm0-44.62Zm0 269.23Z" />
+                    </svg>
+                    <span>View All Customers</span>
+                  </li>
+                  <li className="menu-item">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      height="24px"
+                      viewBox="0 -960 960 960"
+                      width="24px"
+                      fill="#b8b8b8"
+                    >
+                      <path d="M503.85-494.31q25.53-27.77 37.77-63.77 12.23-36 12.23-74.23 0-38.23-12.23-74.23-12.24-36-37.77-63.77 52.69 6.08 87.5 45.5 34.8 39.43 34.8 92.5 0 53.08-34.8 92.5-34.81 39.42-87.5 45.5Zm210 306.62v-93.85q0-32.71-13.31-62.24t-37.77-50.68q46 15.31 84.69 41.31t38.69 71.61v93.85h-72.3ZM786.15-450v-80h-80v-60h80v-80h60v80h80v60h-80v80h-60Zm-452.3-42.31q-57.75 0-98.88-41.12-41.12-41.13-41.12-98.88 0-57.75 41.12-98.87 41.13-41.13 98.88-41.13 57.75 0 98.87 41.13 41.13 41.12 41.13 98.87 0 57.75-41.13 98.88-41.12 41.12-98.87 41.12Zm-300 304.62v-88.93q0-29.38 15.96-54.42 15.96-25.04 42.65-38.5 59.31-29.07 119.66-43.61 60.34-14.54 121.73-14.54 61.38 0 121.73 14.54 60.34 14.54 119.65 43.61 26.69 13.46 42.65 38.5 15.97 25.04 15.97 54.42v88.93h-600Zm300-364.62q33 0 56.5-23.5t23.5-56.5q0-33-23.5-56.5t-56.5-23.5q-33 0-56.5 23.5t-23.5 56.5q0 33 23.5 56.5t56.5 23.5Zm-240 304.62h480v-28.93q0-12.15-7.04-22.5-7.04-10.34-19.12-16.88-51.69-25.46-105.41-38.58-53.73-13.11-108.43-13.11-54.71 0-108.43 13.11Q171.69-341.46 120-316q-12.08 6.54-19.11 16.88-7.04 10.35-7.04 22.5v28.93Zm240-384.62Zm0 384.62Z" />
+                    </svg>
+                    <span>Create Customer</span>
+                  </li>
                 </ul>
               </section>
             </nav>
@@ -71,12 +162,13 @@ export default function Login() {
                 tabIndex="0"
               >
                 <div className="flex flex-row gap-4 p-4">
-                  <div className="avatar-square avatar avatar-md">
+                  <div className="avatar-square avatar avatar-lg">
                     <img src="https://i.pravatar.cc/150?img=30" alt="avatar" />
                   </div>
 
                   <div className="flex flex-col">
-                    <span>Sandra Marx</span>
+                    <span className="text-gray-500">Logged in as:</span>
+                    <span>Full Name</span>
                   </div>
                 </div>
               </label>
@@ -109,44 +201,29 @@ export default function Login() {
         <div className="w-fit">
           <label
             htmlFor="sidebar-mobile-fixed"
-            className="btn-primary btn sm:hidden"
+            className="cursor-pointer sm:hidden"
           >
-            Open Sidebar
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="24px"
+              viewBox="0 -960 960 960"
+              width="24px"
+              fill="#b8b8b8"
+            >
+              <path d="M140-254.62v-59.99h680v59.99H140ZM140-450v-60h680v60H140Zm0-195.39v-59.99h680v59.99H140Z" />
+            </svg>
           </label>
         </div>
 
-        <div className="my-4 grid grid-cols-2 gap-4">
-          <div className="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">
-            +
+        <div className="my-4">
+          <CreateCustomer />
+          {/* <div className="flex h-20 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">
+            <FormField />
           </div>
 
           <div className="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">
             +
-          </div>
-          <div className="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">
-            +
-          </div>
-          <div className="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">
-            +
-          </div>
-          <div className="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">
-            +
-          </div>
-          <div className="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">
-            +
-          </div>
-          <div className="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">
-            +
-          </div>
-          <div className="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">
-            +
-          </div>
-          <div className="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">
-            +
-          </div>
-          <div className="flex h-40 w-full items-center justify-center border-2 border-dashed border-border bg-gray-1">
-            +
-          </div>
+          </div> */}
         </div>
       </div>
     </div>
